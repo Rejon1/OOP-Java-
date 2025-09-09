a0868e413ccaf7f7c03bcdde6639550a546396a3
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 14:58:41 +0600

    Update Lab5.java

--- Lab5.java
+++ Lab5.java
@@ -1,12 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Roudra27
- */
-public class Lab5 {
-    
+public class Lab5 
+{ 
+    public static void main(String[] args)  
+    { 
+        int a = 59;//0101 
+        int b = 90;//1010 
+         
+        System.out.println ("Before Swap: "); 
+        System.out.println("a: " +a); 
+//Q-5: Swap two numbers using bitwise operation (XOR) in Java. 
+        System.out.println("b: " +b); 
+ 
+ 
+         
+        a = a^b;//XOR Sign. 
+        b = a^b; 
+        a = a^b; 
+         
+        System.out.println("After Swap: "); 
+        System.out.println("a: "+a); 
+        System.out.println("b: "+b); 
+    }   
 }
