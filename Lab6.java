50e19af7ff78b081a5c303a4c6eae901ac742911
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 14:57:02 +0600

    Update Lab6.java

--- Lab6.java
+++ Lab6.java
@@ -1,12 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Roudra27
- */
-public class Lab6 {
-    
-}
+public class Lab6
+{ 
+ 
+ 
+    public static void main(String[] args)  
+    { 
+        int a = 345; 
+         
+        if(a%2==0)//Q-6:Print a number and find whether it is Even or Odd. 
+        { 
+            System.out.println("The Printed Number is: Even."); 
+        } 
+        else if(a%2!=0) 
+        { 
+            System.out.println("The Printed Number is: Odd."); 
+        } 
+        else  
+        { 
+            System.out.println("INVALID INPUT"); 
+        } 
+    }   
+} 
