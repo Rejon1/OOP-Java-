0eb6012f5074c24723bb407c368b456aa4164dcb
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 17:31:25 +0600

    Print 'a' to 'z' and break in 'k' using a while loop.

---
+++ Lab12.java
@@ -0,0 +1,24 @@
+public class Lab12  
+{
+public static void main(String[] args)  
+{ 
+ 
+ 
+        double num1 = 1998.5; 
+        double num2 = 77.8; 
+        double num3 = 155.3; 
+        if (num1<=num2 && num1<=num3) 
+        { 
+            System.out.println("The Smallest Number is: " +num1); 
+        } 
+        else if(num2<=num1 && num2<=num3) 
+        { 
+            System.out.println("The Smallest Number is: " +num2); 
+        } 
+        else 
+        { 
+            System.out.println("The Smallest Number is: " +num3); 
+        } 
+    }  
+}
+ 
