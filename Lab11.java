1ed56506494da9c272ac76f8211b20fa236448df
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 17:30:08 +0600

    Print a right-side-down pyramid with 5 to 1 numbers

---
+++ Lab11.java
@@ -0,0 +1,17 @@
+public class Lab11 
+{ 
+    public static void main(String[] args)  
+    { 
+        double number = -25.0; 
+        if(number>0)  
+        { 
+/*2. Write a Java program to check whether a given number is positive, negative, or zero and 
+print the result.*/ 
+System.out.println(number+ " is posive."); 
+} else if (number<0) 
+{ System.out.println(number + " is Negative.");
+} 
+else 
+System.out.println("The number is zero." +number); 
+}     
+} 
