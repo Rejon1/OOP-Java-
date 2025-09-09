14a92978a39a01b789741688e6728946616b003d
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 17:37:07 +0600

    Write a Java program to check whether a value is prime or not.

---
+++ Lab19.java
@@ -0,0 +1,13 @@
+public class Lab19  
+{ 
+public static void main(String[] args)  
+{ 
+int num = 39; 
+
+System.out.println("Quick Price Board: "); 
+for (int i=1; i<= 10; i++) 
+{ 
+    System.out.println(num+ "*" +i+ "="+ (num*i));
+} 
+}     
+} 
