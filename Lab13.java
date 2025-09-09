d2ae685894a002ee438ba742e3baf200ca82d3f1
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 17:32:31 +0600

    Print 'a' to 'z' and break in 'k' using a while loop.

---
+++ Lab13.java
@@ -0,0 +1,16 @@
+public class Lab13  
+{ 
+public static void main(String[] args)  
+{ 
+//Q-10:Print a right-side down pyramid by 1 to 5 numbers.      
+for (int i = 5; i>= 1; i--) 
+{ 
+ for(int j = 1; j <= i; j++) 
+ {
+     System.out.print(j+ " "); 
+ }
+ System.out.println();
+} 
+}
+} 
+
