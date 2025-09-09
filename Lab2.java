70e7c7994d593d94714d5e25ad5ac038ee16a78c
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 12:54:05 +0600

    Print Employee details

---
+++ Lab2.java
@@ -0,0 +1,17 @@
+public class Lab2  
+{ 
+    public static void main(String[] args)  
+    { 
+ 
+ 
+       String name = "Rejon"; 
+       int ID = 1026;//Q-2:Print Employee details (Name, ID, Salary, Annual Salary). 
+       double salary = 200000; 
+       double AnnualSalary = salary*12; 
+ 
+       System.out.println("Name = " +name); 
+       System.out.println ("ID: " +ID); 
+       System.out.println ("Salary = " +salary+ " BDT"); 
+       System.out.println ("Annual Salary = " +AnnualSalary+ " BDT"); 
+    }  
+}
