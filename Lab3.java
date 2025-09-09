b05011b397b955d0cd874493bc3de37845ad67d2
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 14:42:52 +0600

    Swap two variables using a third variable.

---
+++ Lab3.java
@@ -0,0 +1,19 @@
+public class Lab3  
+{ 
+    public static void main(String[] args)  
+ 
+    { 
+        int a = 20; 
+        int b = 31;//Q-3: Swap two variable using a third variable. 
+        int swap; 
+        System.out.println("Before Swapping."); 
+        System.out.println ("a = " +a); 
+        System.out.println ("b = " +b); 
+        swap = a; 
+        a = b; 
+        b = swap; 
+        System.out.println("After Swapping."); 
+        System.out.println ("a = " +a); 
+        System.out.println ("b = " +b); 
+    }   
+}
