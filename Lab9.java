1d9c3bde41f5e755faf3f461569c49016249890e
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 17:28:42 +0600

    Write a Java program to find and print the smallest of three numbers.

---
+++ Lab9.java
@@ -0,0 +1,16 @@
+public class Lab9  
+{ 
+    public static void main(String[] args)  
+    { 
+        int j = 1; 
+         
+        do//Q-9:Print 1 to 10 using a do-while loop. 
+        { 
+            System.out.println("The Number of Output 'j' is: " +j); 
+ 
+ 
+            j++; 
+        } 
+        while(j<=10); 
+    }   
+}
