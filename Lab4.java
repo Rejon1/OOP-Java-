e65d3aaea40ab6ae6a869b6c8eb90f36991378ed
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 14:58:05 +0600

    Update Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -1,12 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Roudra27
- */
-public class Lab4 {
-    
+public class Lab4 
+{ 
+    public static void main(String[] args)  
+    { 
+        int a = 14; 
+        int b = 60;//Q-4: Swap two variable without using a third variable. 
+         
+        System.out.println("Before Swapping."); 
+        System.out.println ("a = " +a); 
+        System.out.println ("b = " +b); 
+         
+        a = a+b; 
+        b = a-b; 
+        a = a-b; 
+         
+        System.out.println("After Swapping."); 
+        System.out.println ("a = " +a); 
+        System.out.println ("b = " +b); 
+    }   
 }
