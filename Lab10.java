955c1591da11a852f64ed010ada7e051f905212a
Rejon1 <rejonahmed345@gmail.com>
2025-09-09 17:29:29 +0600

    Print a right-side-down pyramid with 1 to 5 numbers.

---
+++ Lab10.java
@@ -0,0 +1,13 @@
+public class Lab10  
+{ 
+    public static void main(String[] args)  
+    { 
+       double base = 73.0; 
+ 
+ 
+       double height = 9.0; 
+       double area = 0.5*base*height; 
+        
+       System.out.println("The area of Triangle is: " + area); 
+    }     
+} 
